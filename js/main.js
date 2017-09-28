f9200b81af6b26f3c09740329a4343408937a25f
Jeff Berlin <jeff@jbrally.com>
2017-09-27 22:13:36 -0400

    Adding 'Contact' section with Google Map

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
 var app = angular.module('myApp', [])
-       app.controller('myCtrl', function ($scope, $window) {
-           $scope.OpenFacebookTab = function () {
-               $window.open("https://www.facebook.com/NationalSpeed/");
-           }
+   	app.controller('myCtrl', function ($scope, $window) {
+       $scope.OpenFacebookTab = function () {
+           $window.open("https://www.facebook.com/NationalSpeed/");
+       }
 
-           $scope.OpenInstagramTab = function () {
-           		$window.open("https://www.instagram.com/nationalspeed/");
-           }
+       $scope.OpenInstagramTab = function () {
+       		$window.open("https://www.instagram.com/nationalspeed/");
+       }
 
-           $scope.OpenYoutubeTab = function () {
-           		$window.open("https://www.youtube.com/channel/UCaMdP7NP5nnoMyokjMd089Q/");
-           }
+       $scope.OpenYoutubeTab = function () {
+       		$window.open("https://www.youtube.com/channel/UCaMdP7NP5nnoMyokjMd089Q/");
+       }
 
-           $scope.OpenDynojetTab = function () {
-           		$window.open("http://www.dynojet.com/");
-           }
-       })
+       $scope.OpenDynojetTab = function () {
+       		$window.open("http://www.dynojet.com/");
+       }
+   	})
+
+var map;
+
+// Map location code
+
+function initMap() {
+	var shopLocation = {lat: 34.264266, lng: -77.831065};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 17,
+		center: shopLocation,
+		mapTypeId: 'hybrid'
+	});
+	
+	var contentString = '<div>' + '<h5>National Speed</h5>' + '</div>'; 
+
+	var infowindow = new google.maps.InfoWindow({
+    	content: contentString
+  	});
+
+	var marker = new google.maps.Marker({
+		position: shopLocation,
+		map: map,
+		title: 'National Speed'
+	});
+  
+
+	marker.addListener('click', function() {
+		infowindow.open(map, marker);
+	});
+}
